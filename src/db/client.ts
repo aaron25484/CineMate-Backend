9be6829c6929f5081f4c72e589d52c7ae1d33c11
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-12-21 15:00:54 +0100

    fix: file

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,4 +1,3 @@
-import { PrismaClient as PostgresClient } from "../../prisma/generated/postgresql_client";
 import { PrismaClient as MongoClient } from "../../prisma/generated/mongodb_client";
 import { Prisma } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
@@ -7,21 +6,15 @@ export const DATA_SOURCE: String = process.env.DATA_SOURCE ?? "mongo";
 //export const DATA_SOURCE: String = "postgresql";
 
 type ClientMongo = MongoClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
-type ClientPostgres = PostgresClient<
-  Prisma.PrismaClientOptions,
-  never,
-  DefaultArgs
->;
+
 
 export const mongoClient: ClientMongo = new MongoClient();
-export const postgresClient: ClientPostgres = new PostgresClient();
 
 export let prismaClient: any;
 
 console.log(`DATA_SOURCE before if: ${DATA_SOURCE}`);
 
 if (DATA_SOURCE === "postgresql") {
-  prismaClient = postgresClient;
   console.log("Connecting to PostgreSQL...");
 } else {
   prismaClient = mongoClient;
